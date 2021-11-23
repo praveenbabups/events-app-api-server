4697ca72eed56cb8842d7ea35a5668feef8714d7
praveenbabups <praveenbabups@yahoo.com>
2021-11-23 06:04:21 +0000

    comming new changes

--- src/app.config.js
+++ src/app.config.js
@@ -2,6 +2,7 @@
  * Place to store data specific to this instance
  * DO NOT store secrets here
  * DO NOT store state here - must be part of a stateless service
+ * added new comment line
  */
 class AppConfig {
     version = '1.0.1';
